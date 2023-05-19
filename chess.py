fa862d495d0d22312886b39866f93e4a4b731ed6
Qendrim1 <74424873+Qendrim1@users.noreply.github.com>
2023-05-19 12:03:57 +0200

    Update chess.py

--- chess.py
+++ chess.py
@@ -40,3 +40,46 @@ def solve_n_queens(n, m, depth, MR):
             transforms.append(solution)         # rotation
             transforms.append(solution[::-1])   # vertical reflection
         return transforms
+    
+      def rotate(solution):
+        n = len(solution)
+        rotated = [0] * n
+        for i in range(n):
+            rotated[solution[i]] = n - 1 - i
+        return rotated
+
+    solutions = []
+    place_queens([], m, depth, MR)
+    return solutions
+
+def print_solutions(solutions, n):                              
+    console_size = shutil.get_terminal_size()   # Get the console size
+
+    for solution in solutions:
+        for row in range(n):
+            line = ""
+            for col in range(n):
+                if col == solution[row]:
+                    line += "Q "
+                else:
+                    line += "* "
+            padding = " " * ((console_size.columns - len(line)) // 2)
+            print(padding + line)
+        print("\n")
+    message = "============KY RAST KA {} ZHGJIDHJE============".format(len(solutions))
+    padding = " " * ((console_size.columns - len(message)) // 2)
+    print(padding + message)
+
+n = int(input("Shenoni numrin e dimensioneve te shahut (n*n): "))
+m = int(input("Shenoni numrin e mbretereshave: "))
+depth = int(input("Vleren per depth (Depth for DLS): "))
+MR = input("A doni te perfshini zgjidhjet me pasqyrim dhe rotacion? (po/jo): ")
+start_time = time.time() 
+solutions = solve_n_queens(n, m, depth, MR)
+end_time = time.time()
+print_solutions(solutions, n)
+
+execution_time_message = "Execution time for finding solutions: {} seconds".format(end_time - start_time)
+console_size = shutil.get_terminal_size()
+padding = " " * ((console_size.columns - len(execution_time_message)) // 2)
+print(padding + execution_time_message)
